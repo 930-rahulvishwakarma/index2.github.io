82322255dd8d6497fa55ddaf3b6612dd564d2466
930-rahulvishwakarma <113338505+930-rahulvishwakarma@users.noreply.github.com>
2022-09-24 13:20:47 +0530

    Add files via upload

---
+++ trade.js
@@ -0,0 +1,11 @@
+var Modal = document.getElementById("Modaltrade");
+
+var btntwo = document.getElementById("mybtn");
+var h2 = document.getElementById("closetwo");
+
+btntwo.onclick = function () {
+    Modal.style.display = "block";
+}
+h2.onclick = function () {
+    Modal.style.display = "none";
+}
