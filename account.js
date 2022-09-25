a23f8c441d2a179e1090d3ef46f7d907b6593032
930-rahulvishwakarma <113338505+930-rahulvishwakarma@users.noreply.github.com>
2022-09-25 10:27:27 +0530

    Add files via upload

---
+++ account.js
@@ -0,0 +1,10 @@
+var modalthree = document.getElementById("modalthree");
+var btnthree = document.getElementById("mybtn3");
+var closethree = document.getElementById("closethree");
+
+btnthree.onclick = function(){
+    modalthree.style.display = "block";
+}
+closethree.onclick = function(){
+    modalthree.style.display ="none";
+}
